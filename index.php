8e49cd96a4013791caa885ebeb4cb06d778bde9f
Ignacio2769 <Ignacio2769@DESKTOP-01D67NH>
2019-02-24 12:15:17 +0100

    Ahora he añadido un input

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ and open the template in the editor.
             Clave:
             <input type="password" name="clave"/>
             <input type="submit" name="accion" value="Acceder"/>           
-            
+            <input type="text" placeholder="hola"/>
         </form>
          <br/>
          
